d0ff9ef9620fd4c2f077140fc2f8b7192db603ff
Edilson Silva <edilson_js@yahoo.com.br>
2021-09-16 11:37:44 -0300

    Templates e estruturas

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def main():
-    return "Welcome!"
+    return render_template("index.html")
+
+
+@app.route("/showSignUp")
+def showSignUp():
+    return render_template("signup.html")
 
 
 if __name__ == "__main__":
